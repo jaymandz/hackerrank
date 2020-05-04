a036d4f722279ab72a95fb85c82da9cc0f224b94
Jay Mandane <jvergel.mandane@gmail.com>
2020-05-04 16:05:25 +0800

    Python/Introduction/Print function

---
+++ python/introduction/print_function.py
@@ -0,0 +1,9 @@
+# Score: 20.00 out of 20 points
+# https://www.hackerrank.com/challenges/python-print/problem
+
+# This might be a very small solution, and no one might need to see this, but
+# what the hell.
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1): print(i, end='')
